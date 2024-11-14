59e733ff3af21bc55ec67cb456d68720e5d7c348
TrongPhuc <nguyentrongphuc03@gmail.com>
2024-11-14 13:45:22 +0700

    11/14

--- PetShop/src/main/java/com/example/petshop/rest/RestOrderProductDetailController.java
+++ PetShop/src/main/java/com/example/petshop/rest/RestOrderProductDetailController.java
@@ -21,11 +21,6 @@ public class RestOrderProductDetailController {
     private OrderService orderService;
 
 
-    @PostMapping
-    public OrderProductDetail create(@RequestBody OrderProductDetail orderProductDetail){
-        return orderProductDetailService.save(orderProductDetail);
-    }
-
     @GetMapping("/{id}")
     public List<OrderProductDetail> getAll(@PathVariable("id") Integer id) {
         Order order = orderService.getByOrderId(id);
